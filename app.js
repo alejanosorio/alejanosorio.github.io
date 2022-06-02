dc77a92935e8fae7d8c8892895ea1855307d5a22
alejanosorio <alejoo870@gmail.com>
2022-06-02 11:35:51 -0300

    loader js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 window.addEventListener("load",()=>{
     const contain=document.querySelector(".contain");
-    contain.style.opacity="0";
-    contain.style.visibility="hidden";
+    contain.className+="hidden"
+  
     const hour=document.querySelector(".hour")
     const updateHour=setInterval(() => {
      const date=new Date()
      hour.innerHTML=`${date.getHours()}:${date.getMinutes()}-${date.getFullYear()}`
     }, 1000);
-   	updateHour()
+    
+updateHour()
 
 })
-
-
 
