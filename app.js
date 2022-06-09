5ebd15a83795c88644321c15fc4028e1f43722a3
alejanosorio <alejoo870@gmail.com>
2022-06-09 16:35:20 -0300

    function reloj

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-window.addEventListener("load",()=>{
-    const contain=document.querySelector(".contain");
-    contain.style.display="none"
-  
-
-})
-const hour=document.querySelector(".hour")
-const updateHour=setInterval(() => {
- const date=new Date()
- hour.innerHTML=`${date.getHours()}:${date.getMinutes()}-${date.getFullYear()}`
-}, 1000);
-
-updateHour()
+window.addEventListener("load", () => {
+  const contain = document.querySelector(".contain");
+  contain.style.display = "none";
+});
+const hour = document.querySelector(".hour");
+function updateHour() {
+  setInterval(() => {
+    const date = new Date();
+    hour.innerHTML = `${date.getHours()}:${date.getMinutes()}-${date.getFullYear()}`;
+  }, 1000);
+}
+updateHour();
