1920dd6f519d2225fd3d75321e43c484a10d428b
alejanosorio <alejoo870@gmail.com>
2024-10-17 23:28:43 -0300

    refactorisando 	modificados:     app.js     modificados:     galeria.css 	modificados:     galeria.html 	modificados:     index.html

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 window.addEventListener("load", () => {
-  const contain = document.querySelector(".contain");
-  contain.style.display = "none";
-});
-const hour = document.querySelector(".hour");
-function updateHour() {
-  setInterval(() => {
-    const date = new Date();
-    hour.innerHTML = `${date.getHours()}:${date.getMinutes()}-${date.getFullYear()}`;
-  }, 1000);
+   const contain = document.querySelector(".contain");
+   contain.style.display = "none";
+ });
+function mueveReloj(){
+  momentoActual = new Date()
+  hora = momentoActual.getHours()
+  minuto = momentoActual.getMinutes()
+  segundo = momentoActual.getSeconds()
+
+  str_segundo = new String (segundo)
+  if (str_segundo.length == 1)
+     segundo = "0" + segundo
+
+  str_minuto = new String (minuto)
+  if (str_minuto.length == 1)
+     minuto = "0" + minuto
+
+  str_hora = new String (hora)
+  if (str_hora.length == 1)
+     hora = "0" + hora
+
+  horaImprimible = hora + " : " + minuto + " : " + segundo
+
+  document.form_reloj.reloj.value = horaImprimible
+
+  setTimeout("mueveReloj()",1000)
 }
-updateHour();
